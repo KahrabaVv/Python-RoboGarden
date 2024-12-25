b3a29f621d529fc94b870cda533329e6c9da5e57
Kerolos Sameh El-Shaip Boles <104724781+KahrabaVv@users.noreply.github.com>
2024-12-25 12:54:27 +0200

    Create first.py

---
+++ Python Tutorial/First Module Assessment/first.py
@@ -0,0 +1,8 @@
+# Introduction Program
+name = "Kerolos Sameh El-shaip"
+age = 24  # Replace with your actual age
+goal = "learn Python effectively and develop real-world projects using it."
+
+print("Hello! My name is", name)
+print("I am", age, "years old.")
+print("My goal in this project is to", goal)
