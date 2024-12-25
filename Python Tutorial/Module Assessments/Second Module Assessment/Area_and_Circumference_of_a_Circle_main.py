c90bc025eb2ee4c65c16daa98dacb505fc199717
Kerolos Sameh El-Shaip Boles <104724781+KahrabaVv@users.noreply.github.com>
2024-12-25 13:46:23 +0200

    Update Area_and_Circumference_of_a_Circle_main.py
    Ensure the radius is positive by taking its absolute value

--- Python Tutorial/Module Assessments/Second Module Assessment/Area_and_Circumference_of_a_Circle_main.py
+++ Python Tutorial/Module Assessments/Second Module Assessment/Area_and_Circumference_of_a_Circle_main.py
@@ -1,7 +1,7 @@
 import math  # Import math module for mathematical constants and functions
 
-# Take the input from the user for the radius
-radius = float(input("Enter the radius of the circle: "))
+# Take the input from the user for the radius ,Ensure the radius is positive by taking its absolute value
+radius = abs(float(input("Enter the radius of the circle: ")))
 
 # Calculate the circumference and area
 circumference = 2 * math.pi * radius
